183da4ee564705a91eae02b4dd4e6d9abc483dc7
runtu <huorunquan@126.com>
2021-09-17 17:04:04 +0800

    feat: 完善文档

--- bin/tips.js
+++ bin/tips.js
@@ -17,6 +17,7 @@ dayjs.extend(customParseFormat)
 var isBetween = require('dayjs/plugin/isBetween')
 dayjs.extend(isBetween)
 
+// docs: https://blog.csdn.net/u012981882/article/details/112552450
 const API = 'https://api.apihubs.cn/holiday/get'
 const COMMAND_SHOW = 'show'
 
