950e08aa367b2adbbe166df8f8612c9f6ceb063a
runtu <huorunquan@126.com>
2021-09-17 15:19:33 +0800

    feat: 完成下半年节假日获取逻辑

--- bin/tips.js
+++ bin/tips.js
@@ -13,14 +13,12 @@ dayjs.locale('zh-cn')
 const customParseFormat = require('dayjs/plugin/customParseFormat')
 dayjs.extend(customParseFormat)
 
+const API = 'https://api.apihubs.cn/holiday/get'
 const COMMAND_SHOW = 'show'
 
 function logger () {
   // console.log(typeof arguments)
-  for (let i in arguments) {
-    console.log(chalk.yellow(arguments[i]))
-  }
-  // console.log(arguments.forEach(log => chalk.yellow(log)))
+  console.log(chalk.yellow(...arguments))
 }
 
 function generateDateRange(dateStart, length, unit = 'day') {
@@ -59,25 +57,23 @@ function getTimeDiff (start, end, unit = 'day') {
   return diff
 }
 
-async function showOperation (argv) {
-  const API = 'https://api.apihubs.cn/holiday/get'
-  const today = dayjs().startOf('day')
-  const todayText = today.format('YYYY-MM-DD dddd')
-  console.log(chalk.green('今天是', todayText))
-
+/** 获取下一周末 */
+async function getNextWeekend (today) {
   let nextWeekend = null
 
+  // 获取10日内的下一周末
   const nextWeekendDate = generateDateRange(today, 10, 'day').map(i => i.format('YYYYMMDD'))
   logger('获取下一周末的日期范围', nextWeekendDate)
-  // 获取10日内的下一周末
   const resp = await axios.get(API, {
     params: {
       // 日期范围
       date: nextWeekendDate.join(','),
-      // 只显示周末
-      weekend: 1,
+      // 只显示非工作日
+      workday: 2,
       // 升序
       order_by: 1,
+      // 中文描述
+      cn: 1,
     }
   })
   if (resp.status === 200 && resp.data.code === '0') {
@@ -93,10 +89,62 @@ async function showOperation (argv) {
     logger('网络出错')
   }
 
+  return nextWeekend
+}
+
+/** 获取接下来半年的节假日 */
+async function getHoliday (today) {
+  const holidayArr = []
+
+  const nextHalfYearMonth = generateDateRange(today, 6, 'month').map(i => i.format('YYYYMM'))
+  logger('获取下半年假日的日期范围', nextHalfYearMonth)
+  const resp = await axios.get(API, {
+    params: {
+      // 日期范围
+      month: nextHalfYearMonth.join(','),
+      // 只显示节假日假日
+      holiday_recess: 1,
+      // 升序
+      order_by: 1,
+      // 中文描述
+      cn: 1,
+    }
+  })
+
+  if (resp.status === 200 && resp.data.code === '0') {
+    const baseArr = resp.data.data.list
+    let holidayAnchor
+
+    for (let i = 0, j = baseArr.length; i < j; i += 1) {
+      const item = baseArr[i];
+      if (holidayAnchor === item.holiday) {
+        continue
+      } else {
+        holidayAnchor = item.holiday
+        holidayArr.push([
+          dayjs(`${item.date}`, 'YYYYMMDD'),
+          `${item.holiday_cn}假期`,
+        ])
+      }
+    }
+  }
+
+  return holidayArr
+}
+
+async function showOperation (argv) {
+  const today = dayjs().startOf('day')
+  const todayText = today.format('YYYY-MM-DD dddd')
+  console.log(chalk.green('今天是', todayText))
+
+  const nextWeekend = await getNextWeekend(today)
+  const nextHoliday = await getHoliday(today)
+
   let text = `【摸鱼办公室】${today.format('M月D日')}`
   text += `
 上午好，摸鱼人，工作再累，一定不要忘记摸鱼哦！
 有事没事起身去茶水间去厕所去廊道走走，别老在工位上坐着，钱是老板的，但命是自己的`
+
   if (nextWeekend) {
     const diff = getTimeDiff(today, nextWeekend)
     logger('下一周末与今天的时间差', diff)
@@ -104,6 +152,17 @@ async function showOperation (argv) {
     text += `
 距离本周周末还有${diff}天`
     }
+  }
+
+  if (nextHoliday.length) {
+    nextHoliday.forEach(([date, name]) => {
+      const diff = getTimeDiff(today, date)
+      logger('节日:', name, diff)
+      if (diff > 0) {
+      text += `
+距离${name}还有${diff}天`
+      }
+    })
   }
 
   console.log(chalk.green(text))
--- tests/tips.test.js
+++ tests/tips.test.js
@@ -9,6 +9,13 @@ test('generateDateRange: 2 day', () => {
   expect(result).toEqual(['20211011', '20211012']);
 });
 
+test('generateDateRange: 3 month', () => {
+  const today = dayjs('202111', 'YYYYMM')
+  const result = generateDateRange(today, 3, 'month').map(i => i.format('YYYYMM'))
+
+  expect(result).toEqual(['202111', '202112', '202201']);
+});
+
 test('getTimeDiff: 3 day', () => {
   const start = dayjs('20210917', 'YYYYMMDD')
   const end = dayjs('20210920', 'YYYYMMDD')
