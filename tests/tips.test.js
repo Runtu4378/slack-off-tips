5d1e4a66e7ee4dabe9a66affd16c1393e8696129
runtu <huorunquan@126.com>
2021-11-09 10:35:42 +0800

    feat: 调整文件命名

--- tests/tips.test.js
+++ tests/tips.test.js
@@ -1,25 +1,29 @@
-const dayjs = require('dayjs')
+const dayjs = require('dayjs');
 
-const { generateDateRange, getTimeDiff } = require('../bin/tips');
+const { generateDateRange, getTimeDiff } = require('../bin/slackOffTips');
 
 test('generateDateRange: 2 day', () => {
-  const today = dayjs('20211011', 'YYYYMMDD')
-  const result = generateDateRange(today, 2, 'day').map(i => i.format('YYYYMMDD'))
+  const today = dayjs('20211011', 'YYYYMMDD');
+  const result = generateDateRange(today, 2, 'day').map((i) =>
+    i.format('YYYYMMDD')
+  );
 
   expect(result).toEqual(['20211011', '20211012']);
 });
 
 test('generateDateRange: 3 month', () => {
-  const today = dayjs('202111', 'YYYYMM')
-  const result = generateDateRange(today, 3, 'month').map(i => i.format('YYYYMM'))
+  const today = dayjs('202111', 'YYYYMM');
+  const result = generateDateRange(today, 3, 'month').map((i) =>
+    i.format('YYYYMM')
+  );
 
   expect(result).toEqual(['202111', '202112', '202201']);
 });
 
 test('getTimeDiff: 3 day', () => {
-  const start = dayjs('20210917', 'YYYYMMDD')
-  const end = dayjs('20210920', 'YYYYMMDD')
-  const diff = getTimeDiff(start, end, 'day')
+  const start = dayjs('20210917', 'YYYYMMDD');
+  const end = dayjs('20210920', 'YYYYMMDD');
+  const diff = getTimeDiff(start, end, 'day');
 
-  expect(diff).toBe(3)
-})
+  expect(diff).toBe(3);
+});
